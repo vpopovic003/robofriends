327a340f88451ed8e6512dfadd7a912a14e57759
Vladimir Popovic <vpopovic003@gmail.com>
2023-05-17 15:25:08 +0200

    error boundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots}) => {
+  // in conjuction with ErrorBoundry. Throwing error on purpose.
+  // if (true) {
+  //   throw new Error('NO');
+  // }
   return (
     <div>
       {
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
-
+import ErrorBoundry from '../components/ErrorBoundry';
 
 class App extends Component {
   constructor() {
@@ -35,7 +35,9 @@ class App extends Component {
           <h1>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
           
         </div>
