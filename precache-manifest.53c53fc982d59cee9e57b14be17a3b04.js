a9cf0e93e316733a800b97b4608ac7499ea06232
Vladimir Popovic <vpopovic003@gmail.com>
2023-07-13 14:24:38 +0200

    Updates

---
+++ precache-manifest.53c53fc982d59cee9e57b14be17a3b04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d55f7b7ff399d3ecf0b5",
+    "url": "/robofriends/static/css/main.ed9bacea.chunk.css"
+  },
+  {
+    "revision": "d55f7b7ff399d3ecf0b5",
+    "url": "/robofriends/static/js/main.d55f7b7f.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/robofriends/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "ab4c528e091145f19e00",
+    "url": "/robofriends/static/css/2.c5388665.chunk.css"
+  },
+  {
+    "revision": "ab4c528e091145f19e00",
+    "url": "/robofriends/static/js/2.ab4c528e.chunk.js"
+  },
+  {
+    "revision": "52d67bf84ad6447d96d7",
+    "url": "/robofriends/static/js/runtime~main.52d67bf8.js"
+  },
+  {
+    "revision": "99d6e41636c452a4687053d5de55c8fa",
+    "url": "/robofriends/index.html"
+  }
+];
